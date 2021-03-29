ca07c7922c32369490cde333ec795c5d6f3b8b66
Afshan Aman <afshan@bloom.co>
2021-03-29 13:20:30 -0600

    imported Game Contract

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,16 +1,19 @@
 import {FC} from 'react'
 
 import { useWeb3 } from 'utils/hooks'
+import GameContract from '../../contracts/Game.json'
 
 export const Home: FC = () => {
   const web3 = useWeb3()
 
+
   console.log(web3.defaultAccount)
   console.log(web3.eth.accounts)
+  console.log(web3.eth)
 
   return <div>
     <div>
-      <div>Click <button onClick={createGame}>here</button> create a new game</div>
+      <div>Click <button>here</button> create a new game</div>
 
     </div>
     
